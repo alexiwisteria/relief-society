a5ede9ba72e70d0caa43523221966c861ce876c1
alexiwisteria <153315329+alexiwisteria@users.noreply.github.com>
2025-01-08 21:52:24 -0700

    up to date check

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true, // Disable ESLint checks during builds
     },
-    reactStrictMode: true, // Optional, but good practice
 };
 
 module.exports = nextConfig;
