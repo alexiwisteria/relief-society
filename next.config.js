bd518954ec7b59eb3ec366ebb8330d9397301b83
alexiwisteria <153315329+alexiwisteria@users.noreply.github.com>
2025-01-08 21:48:27 -0700

    Update next.config.js for Dynamic Routing

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true, // Disable ESLint checks during builds
     },
+    reactStrictMode: true, // Optional, but good practice
 };
 
 module.exports = nextConfig;
